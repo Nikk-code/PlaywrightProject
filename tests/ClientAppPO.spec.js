ab19152482ae66f3eb7cdd85b3ffff949bd8b236
Nikk-code <nikkhilsakalkar@gmail.com>
2026-05-26 22:33:45 +0530

    Code updated

--- tests/ClientAppPO.spec.js
+++ tests/ClientAppPO.spec.js
@@ -8,31 +8,31 @@ const { test, expect } = require('@playwright/test');
 const { title, execPath } = require('process');
 const { text } = require('stream/consumers');
 const { POManager } = require('../pageobjects/POManager');
+//Json->string-> js object
+const dataset = JSON.parse(JSON.stringify(require("./utils/placeorderTestData.json")));
+
 
 
 test('Client App Login', async ({ page }) => {
     //js file-Login.js, DashboardPage
     const poManager = new POManager(page);
 
-    const username = "johndoe123477@gmail.com";
-    const password = "John@1234";
-    const productName = 'ZARA COAT 3';
     const products = page.locator(".card-body");
 
     const loginPage = poManager.getLoginPage(page);
 
     await loginPage.goTo();
-    await loginPage.validLogin(username, password);
+    await loginPage.validLogin(dataset.username, dataset.password);
 
     const dashboardPage = poManager.getDashboardPage(page);
 
-    await dashboardPage.searchProducAddCart(productName);
+    await dashboardPage.searchProducAddCart(dataset.productName);
     await dashboardPage.navigateToCart();
 
     // await page.waitForLoadState('networkidle');  // sometimes it's flaky if it's not working we can use below step
     const cartPage = poManager.getCartPage(page);
 
-    await cartPage.VerifyProductIsDisplayed(productName);
+    await cartPage.VerifyProductIsDisplayed(dataset.productName);
     await cartPage.Checkout();
 
     const ordersReviewPage = poManager.getOrdersReviewPage();
@@ -43,8 +43,5 @@ test('Client App Login', async ({ page }) => {
     const ordersHistoryPage = poManager.getOrdersHistoryPage();
     await ordersHistoryPage.searchOrderAndSelect(orderId);
     expect(orderId.includes(await ordersHistoryPage.getOrderId())).toBeTruthy();
-
-
-
 
 });
