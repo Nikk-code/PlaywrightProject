8b4e4f2bc3f6ec6e9dbe45a306d8bcafbe433dd8
Nikk-code <nikkhilsakalkar@gmail.com>
2026-05-27 20:15:54 +0530

    Setting up project configuration for different browsers execution form command line

---
+++ playwright.config1.js
@@ -0,0 +1,63 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+import { trace } from 'console';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+const config = ({
+  testDir: './tests',
+  timeout: 30 * 1000, // This is for overall test timeout if it stucks somewhere will wait till this time  // By default it's 30 sec /* Run tests in files in parallel */
+  expect: {
+    timeout: 5000,   //it will wait till max 5 seconds 
+  },
+  reporter: 'html',
+  use: {
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+
+    browserName: 'webkit',
+    headless: true,
+    screenshot: 'on',
+    trace: 'on'
+
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'safari',
+      use: {
+        browserName: 'webkit',
+        headless: true,
+        screenshot: 'off',
+        trace: 'on', //off,on
+      }
+    },
+
+    {
+      name: 'chrome',
+      use: {
+        browserName: 'chromium',
+        headless: false,
+        screenshot: 'on',
+        trace: 'on', //off,on
+      }
+    }
+  ],
+
+
+});
+
+module.exports = config
+
+// to execute this file we have to give it in command line 
+// npx playwright test tests / ClientAppPo.spec.js --config playwright.config1.js --project=safari
